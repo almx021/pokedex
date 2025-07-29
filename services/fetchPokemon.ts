af2c1b671c38d7364189a0a2348e02b8e5d7c501
André Lucas <andre.mendes@funcap.ce.gov.br>
2025-07-29 16:32:25 -0300

    Adiciona cache para evitar excesso de requisições

--- services/fetchPokemon.ts
+++ services/fetchPokemon.ts
@@ -2,7 +2,7 @@ import { PokemonResponse } from '@/types/pokemonResponse';
 
 export default async function fetchPokemon(pokemon: string): Promise<PokemonResponse | null> {
   try {
-    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`, {cache: 'force-cache'})
     if(!data.ok) {
       return {
         errorCode: data.status,
