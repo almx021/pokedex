bc6fdfcb727f31a837affd2f18a07647d78357c9
André Lucas <andre.mendes@funcap.ce.gov.br>
2025-07-30 11:44:40 -0300

    Adiciona mais informações ao card do pokémon

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
     <div>
       <main>
         <div className="flex items-center justify-center min-h-screen">
-          <div className="w-1/2 min-w-[600px] bg-blue-100 p-6 shadow-md text-center rounded-lg border-red-50">
+          <div className="max-w[900px] min-w-[700px] bg-blue-100 p-6 shadow-md text-center rounded-lg border-red-50">
             <h1 className="text-lg font-bold text-blue-800">Pokédex</h1>
             <p>Busque um pokémon pelo nome ou número da National Pokédex!</p>
             <Search placeholder="Insira nome ou número" onSearch={onSearch} enterButton="Buscar" />
-            <PokemonCard result={typeof value === 'string' ? value : value?.data} />
+            <PokemonCard result={value?.errorText || value?.data} />
           </div>
         </div>
       </main>
--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import { Card } from 'antd'
+import { Card, Carousel } from 'antd'
 import { getColor } from '@/utils/colors'
 
 import type { Pokemon } from '@/types/pokemon'
@@ -13,32 +13,59 @@ const PokemonCard: React.FC<Props> = ({ result }) => {
   if (!result) return null
   if (typeof result === 'string') {
     return (
-      <p className='text-red-500'><strong>Pokémon não encontrado!</strong></p>
+      <p className='text-red-500'><strong>{result}</strong></p>
     )
   }
 
   return (
     <div className="mt-4 flex items-center justify-center">
-      <Card title={<p className='font-bold'>{result.name.charAt(0).toUpperCase()+ result.name.slice(1).toLowerCase()}</p>} 
+      <Card title={<p className='font-bold'>{result.name.charAt(0).toUpperCase() + result.name.slice(1).toLowerCase()}</p>}
         className="shadow-md w-1/2"
-        styles={{ header: { backgroundColor: `${getColor(result.types[0].type.name)}` } }}
+        styles={{ header: { backgroundColor: `${getColor(result.types[0].type.name)}` }}}
         cover={
-          <Image
-            alt='Sprite do Pokémon'
-            src={result.sprites.front_default}
-            width={80}
-            height={80} />
+          <div className='items-center justify-center text-center'>
+            <Image
+              alt='Sprite do Pokémon'
+              src={result.sprites.front_default}
+              width={192}
+              height={192} 
+              className='m-auto'/>
+          </div>
         }
       //  {<img alt='Sprite do Pokémon' src={result.sprites.front_default}/>}
       >
-        <div className='w-full/2 rounded-lg pt-1 pb-1' style={{backgroundColor: `${getColor(result.types[0].type.name)}`}}>
-          <p><strong>Id:</strong> {result.id}</p>
-          <p><strong>Altura:</strong> {result.height / 10}m</p>
-          <p><strong>XP base:</strong> {result.base_experience}</p>
-          <p><strong>Tipos:</strong> {result.types.map((t) => { return t.type.name }).join(', ')}</p>
-        </div>
-        <div>
-
+        <div className='h-200px w-full/2 rounded-lg pt-1 pb-1' style={{ backgroundColor: `${getColor(result.types[0].type.name)}` }}>
+          <Carousel arrows infinite={false} className='pb-7'>
+            <div key={1}>
+              <p><strong>Informações:</strong></p>
+              <div className='flex flex-wrap text-left'>
+                <div className='w-1/2 pl-7'>
+                  <p><strong>Id:</strong> {result.id}</p>
+                  <p><strong>XP base:</strong> {result.base_experience}</p>
+                  <p><strong>Tipos:</strong> {result.types.map((t) => { return t.type.name }).join(', ')}</p>
+                </div>
+                <div className='w-1/2 pl-2'>
+                  <p><strong>Altura:</strong> {result.height / 10}m</p>
+                  <p><strong>Peso:</strong> {result.weight / 10}kg</p>
+                </div>
+              </div>
+            </div>
+            <div key={2}>
+              <p><strong>Status de Combate:</strong></p>
+              <div className='flex flex-wrap text-left'>
+                <div className='w-1/2 pl-7'>
+                  {result.stats.slice(0, 3).map((item, index) => (
+                    <p><strong>{item.stat.name}:</strong> {item.base_stat}</p>
+                  ))}
+                </div>
+                <div className='w-1/2'>
+                  {result.stats.slice(3, 6).map((item, index) => (
+                    <p><strong>{item.stat.name}:</strong> {item.base_stat}</p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </Carousel>
         </div>
       </Card>
     </div>
